2ab024bb846041c73a3c2a7d0578c4bdcdcd0abb
Finns841594 <fengy142857@gmail.com>
2023-10-24 10:58:33 +0200

    rescaled product

--- src/components/ProductUniFi.tsx
+++ src/components/ProductUniFi.tsx
@@ -1,20 +1,15 @@
 import { useGLTF } from '@react-three/drei';
 import { useEffect } from 'react';
-import { Mesh } from 'three';
 
 const ProductUniFi = () => {
   const product = useGLTF('products/UniFi_AP_AC/UniFi_AP_AC_3D_model.gltf');
   useEffect(() => {
     console.log(product);
-    // product.scene.traverse(child => {
-    //   if (
-    //     child instanceof Mesh &&
-    //     child.name.toLowerCase().includes('ceilingnode')
-    //   ) {
-    //     child.visible = false;
-    //   }
-    // });
   }, [product]);
+  const scaleValue = 0.1;
+  if (product.scene) {
+    product.scene.scale.set(scaleValue, scaleValue, scaleValue);
+  }
   return <primitive object={product.scene} />;
 };
 
